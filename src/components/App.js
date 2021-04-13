5a6d8651dbc818eb18fc4e644c7ce3daa86b4bc5
pj <pjhg14@gmail.com>
2021-04-12 20:30:09 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
+const url = "http://localhost:6001/plants/"
+
 function App() {
+  const [plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch(url)
+      .then(resp => resp.json())
+      .then(plantArray => {
+        setPlants(plantArray)
+      })
+  },[])
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant])
+  }
+
   return (
     <div className="app">
       <Header />
-      <PlantPage />
+      <PlantPage plants={plants} handleAddPlant={handleAddPlant}/>
     </div>
   );
 }
