f14a092320bb9ceabb4f5097a9b887068178a39e
pj <pjhg14@gmail.com>
2021-04-12 20:29:56 -0400

    commit tree 57792b78a877314947a1816751d6398529342447

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantPage({ plants, handleAddPlant }) {
+  const [filter, setFilter] = useState("")
+
+  const filteredPlants = plants.filter(plant => {
+    return plant.name.toLowerCase().includes(filter.toLowerCase())
+  })
+
+  function handleFilterChange(input) {
+    setFilter(input)
+  }
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onPlantAdd={handleAddPlant}/>
+      <Search onSearchChange={handleFilterChange}/>
+      <PlantList plants={filteredPlants}/>
     </main>
   );
 }
